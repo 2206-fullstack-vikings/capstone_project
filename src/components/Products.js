ff09db8619c38e0f22db264f43c4c735e147cc47
da-broussard <dustin.a.broussard@gmail.com>
2022-08-11 12:24:06 -0500

    front end react work

---
+++ src/components/Products.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from "react";
+import Axios from "axios";
+
+
+const Products = ()=>{
+    return(
+        
+        <h1>This is where all of our products will be displayed.</h1>
+        
+    
+    )
+}
+
+
+export default Products;
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as App } from './App';
+export {default as Products} from "./Products"
+export {default as Title} from "./Title"
+export {default as Selector} from './Selector'
+export {default as Cart} from './Cart'
+export {default as HomePage} from './Homepage'
