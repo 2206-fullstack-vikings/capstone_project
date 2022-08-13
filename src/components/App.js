045fc99bb3b1bc864922251fc09158c41c288b9d
da-broussard <dustin.a.broussard@gmail.com>
2022-08-13 10:34:00 -0500

    updating code for saturday morning

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
 import { getAPIHealth } from "../axios-services";
-import { Title, HomePage ,Admin, Login} from "./";
+import { Title, HomePage , Admin, Login, SingleProductView} from "./";
 import "../style/App.css";
 
 const App = () => {
@@ -16,36 +17,47 @@ const App = () => {
   const [testState, setTestState]= useState("test");
   const [allProducts, setAllProducts]= useState([]);
   const [userToken, setUserToken]= useState("");
-  const [userLogin, setUserLogin]= useState("")
-  const [userPassword, setUserPassword]= useState("")
+  const [userName, setUserName]= useState("");
+  const [userPassword, setUserPassword]= useState("");
   const [isAdmin, setIsAdmin]= useState(false);
   const [productByTeam, setProductByTeam]= useState([]);
   const [productByDivision,setProductByDivision]=useState([]);
   const [currentUser, setCurrentUser]=useState({});
+  const [email, setEmail]= useState("");
+  const [name, setName]=useState("");
+  const [location, setLocation]=useState("")
 
 
 
 
-  useEffect(() => {
-    // follow this pattern inside your useEffect calls:
-    // first, create an async function that will wrap your axios service adapter
-    // invoke the adapter, await the response, and set the data
-    const getAPIStatus = async () => {
-      const { healthy } = await getAPIHealth();
-      setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
+  // useEffect(() => {
+  //   // follow this pattern inside your useEffect calls:
+  //   // first, create an async function that will wrap your axios service adapter
+  //   // invoke the adapter, await the response, and set the data
+  //   const getAPIStatus = async () => {
+  //     const { healthy } = await getAPIHealth();
+  //     setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
+  //   };
+
+  //   // second, after you've defined your getter above
+  //   // invoke it immediately after its declaration, inside the useEffect callback
+  //   getAPIStatus();
+  // }, []);
+
+  useEffect(()=>{
+    const fetchProducts = async ()=>{
+        try {
+            const response = await axios.get("http://localhost:3000/api/products")
+            const result = response.data
+            setAllProducts(result)
+            
+        } catch (error) {
+            console.error(error)
+        }
     };
-
-    // second, after you've defined your getter above
-    // invoke it immediately after its declaration, inside the useEffect callback
-    getAPIStatus();
-  }, []);
-
-  // return (
-  //   <div className="app-container">
-  //     <h1>Hello, World!</h1>
-  //     <p>API Status: {APIHealth}</p>
-  //   </div>
-  // );
+    fetchProducts() 
+},[])
+  
 
 
   return (
@@ -53,9 +65,21 @@ const App = () => {
     <Title/>
     
       <Routes>
-      <Route path='/' element ={<HomePage allProducts={allProducts} testState={testState} setAllProducts={setAllProducts} />}/>
+      <Route path="/" element ={<HomePage allProducts={allProducts} testState={testState} setAllProducts={setAllProducts} />}/>
       <Route path="/admin" element={<Admin testState={testState}/>}/>
-      <Route path="/login" element={<Login/>}/>
+      <Route path="/login" element={<Login userToken={userToken}
+            setUserToken={setUserToken}
+            userName={userName}
+            userPassword={userPassword}
+            setUserName={setUserName}
+            setUserPassword={setUserPassword}
+            email={email}
+            setEmail={setEmail}
+            name={name}
+            setName={setName}
+            location={location}
+            setLocation={setLocation}/>}/>
+      <Route path="/:id" element={<SingleProductView allProducts={allProducts}/>}/>
      
       </Routes>
       
