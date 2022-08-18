5417a53289b626c287de56109e55b5d9d270f664
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-08-18 12:59:06 -0400

    merge

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,11 +14,7 @@ const Products = (props)=>{
             const response= await axios.post(`http://localhost:3000/api/cart/${event}`)
             const cartItems = response.data.items;
             
-<<<<<<< HEAD
-    
-=======
             
->>>>>>> main
         } catch (error) {
             console.error(error)
         }
